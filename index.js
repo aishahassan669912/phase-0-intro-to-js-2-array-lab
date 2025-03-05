dde3743482476c5b93e8e8a335f7edcfd9d4f164
Aisha Hassan <aishahassan669912@gmail.com>
2025-03-05 04:39:50 -0800

    phase-0-intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+
+  function appendCat (name){
+    return [...cats, name];
+  }
+  function prependCat(name) {
+    return [name, ...cats];}
+
+    function removeLastCat() {
+        return cats.slice(0, -1);
+      }
+      
+      function removeFirstCat() {
+        return cats.slice(1);
+      }
+
+ 
+ 
+ 
+ beforeEach(function (cats) {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
